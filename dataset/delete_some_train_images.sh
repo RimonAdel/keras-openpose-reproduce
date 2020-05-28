85c45de8fabcbbfd65e514d3f589ff09cd38d702
Rimon Adel <35565654+RimonAdel@users.noreply.github.com>
2020-05-28 20:17:43 +0200

    Add files via upload

---
+++ dataset/delete_some_train_images.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "begin"
+cd "train2014"
+for file in *.jpg
+do
+    name=$(echo "$file" | cut -f 1 -d '.')
+    name=$(echo "$name" | tr -dc '0-9')
+    name = $name + 0
+    echo "remove $file"
+
+    # if [ $(($name)) -ge 2014000000050000 ]
+    # then  
+    #     echo "remove $file"
+    #     rm $file
+    # fi
+done
+echo "done"
